c03d49c896b8a78898f2a58216a6a16ae4973085
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-07-21 22:00:58 +0300

    Academy in progress

---
+++ Academy/main.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
+#define HUMAN_GIVE_PARAMETERS last_name, first_name, age
+
+class Human
+{
+private:
+	std::string last_name;
+	std::string first_name;
+	int age;
+
+public:
+	const std::string& get_last_name() const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name() const
+	{
+		return first_name;
+	}
+	int get_age() const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(int age)
+	{
+		this->age = age;
+	}
+
+	Human(HUMAN_TAKE_PARAMETERS)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+
+		cout << "HConstructor: " << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor: " << this << endl;
+	}
+
+	void info() const
+	{
+		cout << last_name << ' ' << first_name << ' ' << age << endl;
+	}
+};
+
+#define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
+
+class Student : public Human
+{
+private:
+	std::string speciality;
+	std::string group;
+	double rating;
+	double attendance;
+
+public:
+	const std::string& get_speciality() const
+	{
+		return speciality;
+	}
+	const std::string& get_group() const
+	{
+		return group;
+	}
+	double get_rating() const
+	{
+		return rating;
+	}
+	double get_attendance() const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) : Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+
+		cout << "SConstructor: " << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor: " << this << endl;
+	}
+
+	void info() const
+	{
+		Human::info();
+		cout << speciality << ' ' << group << ' ' << attendance << endl;
+	}
+};
+
+#define TEACHER_TAKE_PARAMETERS const std::string& speciality, int experience
+#define TEACHER_GIVE_PARAMETERS speciality, experience
+
+class Teacher : public Human
+{
+private:
+	std::string speciality;
+	int experience;
+
+public:
+	const std::string& get_speciality() const
+	{
+		return speciality;
+	}
+	int get_experience() const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(int experience)
+	{
+		this->experience = experience;
+	}
+
+	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS) : Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+
+		cout << "TConstructor: " << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor: " << this << endl;
+	}
+
+	void info() const
+	{
+		Human::info();
+		cout << speciality << ' ' << experience << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Human human("Montana", "Antonio", 25);
+	human.info();
+
+	Student student("Pincman", "Jessie", 22, "Chemistry", "WW_240", 99, 95);
+	student.info();
+
+	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
+	teacher.info();
+
+	return 0;
+}
